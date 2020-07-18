92b9b442c9028ac87fc07228148d15ca0e6b7dee
Varsha Singh <varshasingh22111997@gmail.com>
2020-07-19 01:03:59 +0530

    Portfolio

--- js/script.js
+++ js/script.js
@@ -105,3 +105,29 @@ function showDivs4(n) {
   x[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " w3-opacity-off";
 }
+
+/*========================================ACHEIVEMENT================================================*/
+
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip1").click(function(){
+    $("#panel1").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip2").click(function(){
+    $("#panel2").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip3").click(function(){
+    $("#panel3").slideToggle("slow");
+  });
+});
